d9c4f2ff86efd06b8e0c49d80e151b03a35b28b5
Somaia Khalil <sumaya_khair@live.com>
2020-01-22 05:22:03 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,39 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(path)
+  emo = YAML.load_file(path)
+  newhash={
+    'get_meaning' => {},
+    'get_emoticon' => {}
+  }
+  emo.each do |key,value|
+      newhash['get_meaning'][value[1]]= key
+      newhash['get_emoticon'][value[0]] = value[1]
+    end
+    newhash
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+def get_japanese_emoticon(file, e_emoticon)
+  lookup = load_library(file)
+  j_emoticon = lookup['get_emoticon'][e_emoticon]
+        
+        if j_emoticon
+          j_emoticon
+        else
+           "Sorry, that emoticon was not found"
+        end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning (file, j_emoticoon)
+  lookup = load_library(file)
+  e_emoticon = lookup['get_meaning'][j_emoticoon]
+  if e_emoticon
+    e_emoticon
+  else
+               "Sorry, that emoticon was not found"
+  end
 end
+
+{"angel"=>["O:)", "☜(⌒▽⌒)☞"], "angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"], "bored"=>[":O", "(ΘεΘ;)"], "confused"=>["%)", "(゜.゜)"], "embarrased"=>[":$", "(#^.^#)"], "fish"=>["><>", ">゜))))彡"], "glasses"=>["8D", "(^0_0^)"], "grinning"=>["=D", "(￣ー￣)"], "happy"=>[":)", "(＾ｖ＾)"], "kiss"=>[":*", "(*^3^)/~☆"], "sad"=>[":'(", "(Ｔ▽Ｔ)"], "surprised"=>[":o", "o_O"], "wink"=>[";)", "(^_-)"]}
